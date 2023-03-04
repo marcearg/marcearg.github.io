28a48307ea62b1c7774372652cc23ea02c018913
marcearg <126556372+marcearg@users.noreply.github.com>
2023-03-04 01:36:13 -0300

    Initial commit

---
+++ js/function.js
@@ -0,0 +1,9 @@
+document.getElementById('Gris').addEventListener('click', function(){
+    document.body.style.backgroundColor='grey';
+    document.getElementById('nombrePrincipal').style.color = 'white';
+});
+
+document.getElementById('Blanco').addEventListener('click', function(){
+    document.body.style.backgroundColor='white';
+    document.getElementById('nombrePrincipal').style.color = '#dc3545';
+});
